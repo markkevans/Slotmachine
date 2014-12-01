c83df6e8d626f186b7c08dae8f3d184127c224a0
Mark Evans <markkevans@gmail.com>
2014-12-01 23:41:30 +0000

    Make the slots work

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Mark Evans on 01/12/2014.
+//  Copyright (c) 2014 Mark Evans. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
